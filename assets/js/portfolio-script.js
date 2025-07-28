cfff6ef9c86acfa23ef8360e87d89fd47c2bcb76
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-07-28 12:16:57 -0700

    js script for portfolio added

---
+++ assets/js/portfolio-script.js
@@ -0,0 +1,16 @@
+// portfolio
+document.addEventListener("DOMContentLoaded", function () {
+    const tabs = document.querySelectorAll(".tab");
+    const galleries = document.querySelectorAll(".tab-content");
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            tabs.forEach((t) => t.classList.remove("active"));
+            galleries.forEach((g) => g.classList.add("hidden"));
+
+            tab.classList.add("active");
+            document.getElementById(tab.dataset.tab).classList.remove("hidden");
+        });
+    });
+});
+console.log("Script loaded");
--- assets/js/script.js
+++ assets/js/script.js
@@ -40,23 +40,6 @@ contactMethods.forEach((radio, index) => {
     });
 });
 
-// portfolio
-document.addEventListener("DOMContentLoaded", function () {
-    const tabs = document.querySelectorAll(".tab");
-    const galleries = document.querySelectorAll(".tab-content");
-
-    tabs.forEach((tab) => {
-        tab.addEventListener("click", () => {
-            tabs.forEach((t) => t.classList.remove("active"));
-            galleries.forEach((g) => g.classList.add("hidden"));
-
-            tab.classList.add("active");
-            document.getElementById(tab.dataset.tab).classList.remove("hidden");
-        });
-    });
-});
-console.log("Script loaded");
-
 // submit form
 
 form.addEventListener("submit", function (event) {
